17df7994d59f343e1d44ebb5553ac3b60d586b65
myql <276466206@qq.com>
2017-01-19 21:09:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
+import bar from './bar';
 import Vue from 'vue'
-
 var app = new Vue({
   el: '#app',
   data: {
-    newTodo: '',
-    todoList: []
-  },
-  created: function(){
-  	window.onbeforeunload=()=>{
-  		let dataString=JSON.stringify(this.todoList)
-  		window.localStorage.setItem('myTodos',dataString)
-  	}
-
-  	let oldDataString=window.localStorage.getItem('myTodos')
-  	let oldData=JSON.parse(oldDataString)
-  	this.todoList=oldData||[]
-  },
-  methods: {
-    addTodo: function(){
-      this.todoList.push({
-        title: this.newTodo,
-        createdAt: new Date(),
-        done: false
-      })
-      this.newTodo = ''
-    },
-    removeTodo: function(todo){
-    	let index=this.todoList.indexOf(todo)
-    	this.todoList.splice(index,1)
-    }
+    message: 'Hello World!'
   }
-})   
+})     
